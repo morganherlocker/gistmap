5258a9573cc536fa2fa5109c8e44d7f0a986692e
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-02-04 16:24:50 -0500

    added leaflet map

---
+++ README.md
@@ -0,0 +1,7 @@
+gistmap
+=======
+
+web app that creates an interactive map from a github repo
+
+
+http://morganherlocker.github.io/gistmap
--- js/gistmap.js
+++ js/gistmap.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+$(function(){
+  // create a map in the "map" div, set the view to a given place and zoom
+  var map = L.map('map').setView([51.505, -0.09], 13);
+
+  // add an OpenStreetMap tile layer
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+})
